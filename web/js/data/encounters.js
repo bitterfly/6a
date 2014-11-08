da25bff492634541e1369337d2c4c6f54b065abb
rherince <rherince@gmail.com>
2014-11-08 12:46:42 +0200

    This are the javascripts with the global variables, where we will store the data for the encounters and the memories

---
+++ web/js/data/encounters.js
@@ -0,0 +1,102 @@
+/**
+ * Created by Rali on 8.11.2014 г..
+ */
+
+var enounters = {
+    0: {
+        range: {
+            start: 2,
+            end: 10
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    },
+    1: {
+        range: {
+            start: 2,
+            end: 10
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    },
+    3: {
+        range: {
+            start: 10,
+            end: 16
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    },
+    4: {
+        range: {
+            start: 10,
+            end: 16
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    },
+    5: {
+        range: {
+            start: 16,
+            end: 21
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    },
+    6: {
+        range: {
+            start: 16,
+            end: 21
+        },
+        images: [],
+        text: '',
+        choices: [
+            {
+
+            },
+            {
+
+            }
+        ]
+    }
+} ;
---
+++ web/js/data/memories.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Rali on 8.11.2014 г..
+ */
+
+var memories = {
+    0: {
+        range: 2,
+        effect: function () {}
+    },
+    1: {
+        range: 2,
+        effect: function () {}
+    },
+    3: {
+        range: 2,
+        effect: function () {}
+    },
+    4: {
+        range: 2,
+        effect: function () {}
+    },
+    5: {
+        range: 2,
+        effect: function () {}
+    },
+    6: {
+        range: 2,
+        effect: function () {}
+    }
+} ;
