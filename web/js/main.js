384653fff0cb05856845eec23a8c71b62d767134
Angel Angelov <hextwoa@gmail.com>
2014-11-08 15:55:02 +0200

    cur

--- web/js/main.js
+++ web/js/main.js
@@ -3,13 +3,13 @@ function glob() {
 }
 
 function mouse(x, oldx) {
-    glob().room_frames[oldx - 1].hide();
-    glob().room_frames[x - 1].show();
+    CUR.room_frames[oldx - 1].hide();
+    CUR.room_frames[x - 1].show();
 }
 
 $(document).ready(function() {
     window.globals_6a = {};
-    glob().mousex = 20;
+    CUR.mousex = 20;
 
     $("body").mousemove(function(ev) {
         var x = Math.ceil(ev.pageX * 40 / $(window).width());
@@ -18,11 +18,11 @@ $(document).ready(function() {
         } else if (x > 40) {
             x = 40;
         }
-        if (glob().mousex != x) {
-            mouse(x, glob().mousex);
-            glob().mousex = x;
+        if (CUR.mousex != x) {
+            mouse(x, CUR.mousex);
+            CUR.mousex = x;
         }
     });
     
-    glob().room_frames = add_images(ROOM_IMAGES, $("#room_background"));
+    CUR.room_frames = add_images(ROOM_IMAGES, $("#room_background"));
 });
