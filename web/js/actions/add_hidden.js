7ab8f027d9cc041ad0c2d23dbf7313278f8bd79e
rherince <rherince@gmail.com>
2014-11-08 14:44:23 +0200

    changed a few names

--- web/js/actions/add_hidden.js
+++ web/js/actions/add_hidden.js
@@ -2,7 +2,7 @@
  * Created by Rali on 8.11.2014 г..
  */
 
-function ADD_HIDDEN_IMAGE (data, divId) {
+function ADD_HIDDEN (data, divId) {
     var htmlstring = '';
 
     htmlstring += '<img src="' + data.src + '">\n';
