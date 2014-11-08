ecad194cef3846148b2929eca827836c2b930991
rherince <rherince@gmail.com>
2014-11-08 21:37:28 +0200

    editing pop quiz stuff

--- web/js/actions/top_quiz.js
+++ web/js/actions/top_quiz.js
@@ -2,22 +2,116 @@
  * Created by Rali on 8.11.2014 г..
  */
 
-function POP_QUIZ () {
+QUIZES = {
+    0: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    1: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    3: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    4: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    5: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    6: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    },
+    7: {
+        question: {
+            type: '',
+            text: '',
+            image: ''
+        },
+        choices: [],
+        effect: function () {}
+    }
+} ;
+
+function POP_QUIZ (index) {
 
     $BODY.append('<div id="pop_quiz"></div>');
 
-    var innerHtml = '';
+    /**
+     * this adds the quiz stuff to the html
+     * @type {string}
+     */
 
-    innerHtml += '<div id="question"></div>';
+    var innerHtml;
+    innerHtml= '';
 
-    innerHtml += '<div class="quiz-timer-progress-bar-div">' +
-        '<hr class="quiz-timer-progress-bar" style="width: 0.0%;"></div>';
+    var temp_type;
+
+    temp_type = QUIZES[index][question][type];
+    if (temp_type == 'text') {
+        innerHtml += '<div id="question">' + QUIZES[index][question][text] + '</div>';
+    } else {
+        innerHtml += '<div id="question"><img src="' + QUIZES[index][question][image] + '"></div>';
+    }
+
+    var choices;
+    choices = '';
+
+    $.each(QUIZES[index][choices], function () {
+        choices += '<li>' + this + '</li> \n';
+    });
+
+    innerHtml += '<div id="choices_container"><ul>' + choices + '</ul></div>';
+
+    innerHtml += '<hr class="quiz-timer-progress-bar" style="width: 0.0%;">';
 
     $('#pop_quiz').append(innerHtml);
 
-    function question(src) {
+    /**
+     * this shows the question
+     * @param src
+     */
 
-    }
+    var choice = undefined;
+
+    /**
+     * this makes stuff with the timebar
+     */
 
     function timebar() {
         var percent = 100, bar = $('.quiz-timer-progress-bar'), timebar = $('#timebar');
@@ -25,10 +119,19 @@ function POP_QUIZ () {
         function progressBarCarousel() {
             bar.css({width:percent+'%'});
             percent = percent -0.5;
-            if (percent == 0) {
-                function stuffEnds () {};
+            if (percent == 0 || choice != undefined) {
+                if (percent == 0 && choice === undefined) {
+                    /*conole.log('times up!');*/
+                } else {
+                    /*console.log('stuff happens');*/
+                }
             }
-
         }
+    }
+
+    QUIZES[index][choices].click(onChoice);
+
+    function onChoice() {
+
     }
 }
