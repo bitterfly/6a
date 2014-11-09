4a3768bdcc444a08765eb7e68a7a3b62bf6eb2b2
rgeorgiev583 <rgeorgiev583@gmail.com>
2014-11-09 16:09:24 +0200

    fixed tree again

--- web/js/skill-tree.js
+++ web/js/skill-tree.js
@@ -1,27 +1,27 @@
-var SkillTree = Class.create(BinaryTree, {
+"use strict";
 
-    initialize: function($super, source) {
-        this.dict = {};
-        $super(source, true, function(value) { dict[value.name] = value; });
-        this.points = 0;
-    },
+function SkillTree(source) {
+    this.dict = {};
+    BinaryTree.call(this, source, true, function(value) { this.dict[value.name] = value; });
+    this.points = 0;
+}
 
-    master: function(skill) {
-        var skill_node = find_rec(skill); 
+SkillTree.prototype = new BinaryTree;
 
-        if (!skill_node.mastered && this.points > skill_node.price) {
-            this.points -= skill_node.price;
-            skill_node.mastered = true;
-        }
-    },
+SkillTree.prototype.master = function(skill) {
+    var skill_node = this.find_rec(skill); 
 
-    add_points: function(points) {
-        this.points += points;
-    },
-
-    find: function(name) {
-        return dict[name];
+    if (!skill_node.mastered && this.points > skill_node.price) {
+        this.points -= skill_node.price;
+        skill_node.mastered = true;
     }
+}
 
-});
- 
+SkillTree.prototype.add_points = function(points) {
+    this.points += points;
+}
+
+SkillTree.prototype.find = function(name) {
+    return this.dict[name];
+}
+
